4c6bcbe3461c81c213b279922412f4e9b9aab09d
arezhz <arezhz@gmail.com>
2023-01-20 20:46:20 +0330

    auth component router

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -13,11 +13,14 @@ export class AuthComponent {
   isLogin = false;
 
   constructor(private router: Router) {
+    this.isLogin = this.router.url.includes('login');
     this.router.events
       .pipe(untilDestroyed(this))
       .subscribe((res: any) => {
         if (res instanceof NavigationEnd) {
-          this.isLogin = res.url.includes('login');
+          this.isLogin = res.urlAfterRedirects
+            ? res.urlAfterRedirects.includes('login')
+            : res.url.includes('login');
         }
       })
   }
